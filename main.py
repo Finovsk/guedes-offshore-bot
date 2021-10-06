d871149ccedd8ca17d77174dce79964c5b132a07
Matheus Siqueira <matsansiqueira@gmail.com>
2021-10-06 16:05:35 -0300

    Add comments and rename some stuff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 import requests
 import tweepy
-import logging
 import os
 
 def get_total():
@@ -11,52 +10,41 @@ def get_total():
     quote_json = json.loads(quote.content)
     total = 9500000*float(quote_json["USDBRL"]["ask"])
     total_currency = "{:,.2f}".format(total)
-    logging.info("Total: R${}!".format(total_currency))
     return total_currency
 
-
 def publish_tweet(consumer_key, consumer_secret, key, secret, total):
-    logging.info("Start to publish tweet!")
+    # Source http://www.yahii.com.br/dolardiario19.html
+    initial = 9500000.0 * 3.8595
+    total_revenue = float(total.replace(",","")) - initial
     
-    inicial = 9500000.0 * 3.8595
-    rendimento_total = float(total.replace(",","")) - inicial
-
+    # Authenticate tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(key, secret)
-
     api = tweepy.API(auth)
-    
+
+    # !!!Warning!!! Gambiarra ahead!
+    # Set latest total value from latest tweet. (No API found)
     tweets = api.user_timeline(screen_name='guedesoffshore',count=1)
     latest_tweet = str(tweets[0])
-    valor = latest_tweet.partition("R$")[2]
-    valor = valor.partition("!")[0]
-    
-    dif = float(total.replace(",","")) - float(valor.replace(",",""))
+    latest_value = latest_tweet.partition("R$")[2]
+    latest_value = latest_value.partition("!")[0]
 
+    dif = float(total.replace(",","")) - float(latest_value.replace(",",""))
     if dif > 0:
-        logging.info("Setting profit tweet.")
-        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nRendeu hoje R${:,.2f}.\nRendimento total R${:,.2f}".format(total, inicial, dif, rendimento_total)
-        media = api.media_upload("feliz.jpg")
+        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nRendeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, initial, dif, total_revenue)
+        media = api.media_upload("rendeu.jpg")
     else:
-        logging.info("Setting loss tweet.")
-        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nPerdeu hoje R${:,.2f}.\nRendimento total R${:,.2f}".format(total, inicial, dif, rendimento_total)
-        media = api.media_upload("triste.jpg")
-    
+        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nPerdeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, initial, dif, total_revenue)
+        media = api.media_upload("perdeu.jpg")
     api.update_status(status=tweet, media_ids=[media.media_id])
 
 def main():
-
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     key = os.environ['KEY']
     secret = os.environ['SECRET']
-
-    logging.basicConfig(level=logging.INFO,format="%(asctime)s:%(levelname)s:%(message)s")
-
     total = get_total()
-
     publish_tweet(consumer_key, consumer_secret, key, secret, total)
-    logging.info("Tweeted!")
 
 if __name__ == "__main__":
     main()
