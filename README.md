a7472f33ce077753a9ce7aeca7ceb342e506947f
André Balbi <amabalbi@gmail.com>
2021-10-26 21:39:36 -0300

    Refactoring and optimizing redundant code. Also documented methods and commented important steps!

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ docker built -t finovsk/guedes-offshore-bot .
 ```sh
 docker run -v $(pwd):/app/ -e CONSUMER_KEY=YYY -e CONSUMER_SECRET=YYY -e KEY=YYY -e SECRET=YYY finovsk/guedes-offshore-bot
 ```
+### Fontes
+
+Cotações dólar-real antigas: http://www.yahii.com.br/dolardiario19.html
+Cotação dólar-real atual: https://economia.awesomeapi.com.br/json/last/USD-BRL
+Valor inicial do investimento:  USD 9.500.000,00 ~ R$ 36.665.250,00 
--- main.py
+++ main.py
@@ -3,48 +3,61 @@ import requests
 import tweepy
 import os
 
+
+# global variables
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+KEY = os.environ['KEY']
+SECRET = os.environ['SECRET']
+STARTING_CAPITAL = 9500000
+STARTING_CAPITAL_IN_BRL = STARTING_CAPITAL * 3.8595
+QUOTE_LINK = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+
+# methods
+
 def get_total():
-    quote = requests.get(
-        'https://economia.awesomeapi.com.br/json/last/USD-BRL'
-    )
-    quote_json = json.loads(quote.content)
-    total = 9500000*float(quote_json["USDBRL"]["ask"])
-    total_currency = "{:,.2f}".format(total)
-    return total_currency
+    """This function gets actual dollar quote from QUOTE_LINK"""
+    
+    actual_dolar_quote = requests.get(QUOTE_LINK).content
+    quote_json_object = json.loads(actual_dolar_quote)
+    value_in_brl = STARTING_CAPITAL * float(quote_json_object["USDBRL"]["ask"])
+    formatted_value_in_brl = "{:,.2f}".format(value_in_brl)
+    
+    return formatted_value_in_brl
 
 def publish_tweet(consumer_key, consumer_secret, key, secret, total):
-    # Source http://www.yahii.com.br/dolardiario19.html
-    initial = 9500000.0 * 3.8595
-    total_revenue = float(total.replace(",","")) - initial
+    """This function publishes a new tweet with gathered data."""
     
-    # Authenticate tweepy
+    total_revenue = float(total.replace(",","")) - STARTING_CAPITAL_IN_BRL
+    
+    # Authenticate tweepy access token with Twitter API
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(key, secret)
     api = tweepy.API(auth)
 
-    # !!!Warning!!! Gambiarra ahead!
-    # Set latest total value from latest tweet. (No API found)
+    # !!!Warning!!! Gambiarra ahead! 
+    # Set latest total value using data from latest tweet. (No API found, have you thought about using requests with latest tweet link?)
+
     tweets = api.user_timeline(screen_name='guedesoffshore',count=1)
     latest_tweet = str(tweets[0])
-    latest_value = latest_tweet.partition("R$")[2]
-    latest_value = latest_value.partition("!")[0]
+    latest_value = latest_tweet.partition("R$")[2].partition("!")[0]
 
-    dif = float(total.replace(",","")) - float(latest_value.replace(",",""))
-    if dif > 0:
-        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nRendeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, initial, dif, total_revenue)
+    difference_from_last_quotation = float(total.replace(",","")) - float(latest_value.replace(",",""))
+    
+    if difference_from_last_quotation > 0:
+        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nRendeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, STARTING_CAPITAL_IN_BRL, difference_from_last_quotation, total_revenue)
         media = api.media_upload("rendeu.jpg")
     else:
-        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nPerdeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, initial, abs(dif), total_revenue)
+        tweet = "Total R${}!\nEm 02/01/2019, o valor era R${:,.2f}.\nPerdeu hoje R${:,.2f}.\nRendimento total R${:,.2f}.".format(total, STARTING_CAPITAL_IN_BRL, abs(difference_from_last_quotation), total_revenue)
         media = api.media_upload("perdeu.jpg")
     api.update_status(status=tweet, media_ids=[media.media_id])
 
 def main():
-    consumer_key = os.environ['CONSUMER_KEY']
-    consumer_secret = os.environ['CONSUMER_SECRET']
-    key = os.environ['KEY']
-    secret = os.environ['SECRET']
     total = get_total()
-    publish_tweet(consumer_key, consumer_secret, key, secret, total)
+    publish_tweet(CONSUMER_KEY, CONSUMER_SECRET, KEY, SECRET, total)
+
+# main
 
 if __name__ == "__main__":
     main()
